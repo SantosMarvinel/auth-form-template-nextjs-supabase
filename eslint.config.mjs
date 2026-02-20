9167c20dcdc08875f99252f776e5fc6eb59d0e70
SantosMarvinel <santos.marvinel@gmail.com>
2026-02-20 09:26:23 +0800

    setting up starting set up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,44 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import checkFile from "eslint-plugin-check-file";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettier,
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{jsx,tsx}": "KEBAB_CASE",
+          "**/*.{js,ts}": "KEBAB_CASE",
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "src/app/(**)",
   ]),
 ]);
 
